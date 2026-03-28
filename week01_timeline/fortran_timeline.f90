a4c882367b710650d9bbda8f614c063da263461d
Wyatt Scherer <wscherer25@gmail.com>
2026-03-28 12:02:33 -0400

    Added simple program printing out fortran timeline

---
+++ week01_timeline/fortran_timeline.f90
@@ -0,0 +1,11 @@
+program fortran_timeline
+    implicit none
+    print *, "1957 - FORTRAN I: first widely used high-level scientific language"
+    print *, "1960 - FORTRAN II: added subroutines and functions"
+    print *, "1962 - FORTRAN IV: added structured programming constructs"
+    print *, "1978 - FORTRAN 77: added character data type and improved control structures"
+    print *, "1991 - Fortran 90: added modules, array programming, and dynamic memory allocation"
+    print *, "2003 - Fortran 2003: added object-oriented programming features"
+    print *, "2018 - Fortran 2018: added coarrays for parallel programming and other modern features"
+    
+end program fortran_timeline
