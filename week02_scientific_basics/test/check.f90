674fa7a0751fb828b93b007501f262d24a57206f
Wyatt Scherer <wscherer25@gmail.com>
2026-04-05 10:12:44 -0400

    Added initial test assert subroutine

--- week02_scientific_basics/test/check.f90
+++ week02_scientific_basics/test/check.f90
@@ -1,5 +1,25 @@
 program check
-implicit none
+    use scientific_basics, only: square
+    implicit none
 
-print *, "Put some tests in here!"
+    print *, "Running tests."
+
+    call test_square_positive()
+    call test_square_zero()
+    call test_square_negative()
+
+    print *, "All tests passed."
+
+contains
+
+    subroutine assert_close(actual, expected, tol, msg)
+        real, intent(in) :: actual, expected, tol
+        character(len=*), intent(in) :: msg
+        if (abs(actual - expected) > tol) then
+            print *, "FAIL:", trim(msg)
+            print *, " expected:", expected, " actual:", actual
+            error stop 1
+        end if
+    end subroutine assert_close
+
 end program check
