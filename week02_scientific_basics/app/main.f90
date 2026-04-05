a17c8c8c6d8f7c8c4f0997563e2c146bdaa8ea00
Wyatt Scherer <wscherer25@gmail.com>
2026-04-05 09:54:22 -0400

    Update main to call the square function and ask for terminal input

--- week02_scientific_basics/app/main.f90
+++ week02_scientific_basics/app/main.f90
@@ -1,6 +1,19 @@
 program main
-  use scientific_basics, only: say_hello
+  use scientific_basics, only: square
   implicit none
 
-  call say_hello()
+  real :: x, y
+  integer :: ios
+
+  write(*,'(A)', advance="no") "Enter a single real number: "
+  read(*,*, iostat=ios) x
+
+  ! check if input read is valid
+  if (ios /= 0) then
+    print *, "Invalid input. Please enter a real number."
+    stop 1
+  end if
+
+  y = square(x)
+  print *, "Square = ", y
 end program main
